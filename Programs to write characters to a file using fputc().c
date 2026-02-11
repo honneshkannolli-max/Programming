d9a8341277b0dad2b5e2554f39cd828eb818f694
honneshkannolli-max <honneshkannolli@gmail.com>
2026-02-11 13:16:29 +0530

    Add files via upload

---
+++ Programs to write characters to a file using fputc().c
@@ -0,0 +1,16 @@
+//programs to write characters to a file using fputc()
+#include<stdio.h>
+int main(){
+FILE *fp;
+fp=fopen("charfile.txt","w");
+if(fp==NULL){
+printf("Unable to open file!\n");
+return 1;
+}
+fputc('A',fp);
+fputc('\n',fp);
+fputc('B',fp);
+fclose(fp);
+printf("Characters written successfully.\n");
+return 0;
+}
